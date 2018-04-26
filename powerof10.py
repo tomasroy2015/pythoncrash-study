06404c8254b81bc659fa152d05b6eed5ec3c18d8
Roy <tomas.cse.ju@gmail.com>
2018-04-26 10:20:55 +0900

    test

--- powerof10.py
+++ powerof10.py
@@ -1,9 +1,12 @@
 n = int(input("Enter input number="))
-while n%10 == 0:
-    n = n/10
-    print(n)
-            
-if(n == 1):
-    print("Its a power of 10")
+if(n<=0):
+    print("Invalid input")
 else:
-    print("Not power of 10")
+    while n%10 == 0:
+        n = n/10
+        print(n)
+                
+    if(n == 1):
+        print("Its a power of 10")
+    else:
+        print("Not power of 10")
